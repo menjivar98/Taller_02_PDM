7fdb11a6647450f6002bd8d064c16753b15068ad
Carlos Ruiz <00095417@uca.edu.sv>
2019-04-11 20:45:15 -0600

    trabajando menu lateral

--- app/src/main/java/com/example/taller_02_pdm/MainActivity.kt
+++ app/src/main/java/com/example/taller_02_pdm/MainActivity.kt
@@ -2,11 +2,47 @@ package com.example.taller_02_pdm
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Parcel
+import android.os.Parcelable
+import kotlinx.android.synthetic.main.app_main_bar.*
+import android.support.design.widget.Snackbar
+import android.support.design.widget.NavigationView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity() : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    var twoPane = false
+
+    constructor(parcel: Parcel) : this() {
+        twoPane = parcel.readByte() != 0.toByte()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setSupportActionBar(toolbar)
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeByte(if (twoPane) 1 else 0)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<MainActivity> {
+        override fun createFromParcel(parcel: Parcel): MainActivity {
+            return MainActivity(parcel)
+        }
+
+        override fun newArray(size: Int): Array<MainActivity?> {
+            return arrayOfNulls(size)
+        }
     }
 }
